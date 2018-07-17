985f8af33cb4b42e3da9b16726a1846f6e70d472
xdfm1 <40327925+xdfm1@users.noreply.github.com>
2018-07-17 22:16:55 +0100

    removing exercise 6 files

--- exercises/lesson06/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -v $(pwd):/app -w /app smatyukevich/raspberry-pi-os-builder make $1
--- exercises/lesson06/include/timer.h
+++
@@ -1,7 +0,0 @@
-#ifndef	_TIMER_H
-#define	_TIMER_H
-
-void timer_init ( void );
-void handle_timer_irq ( void );
-
-#endif  /*_TIMER_H */
