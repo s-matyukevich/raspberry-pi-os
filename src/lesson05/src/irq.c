9944f92436c6f09d140b95c4c2a8d7810936de93
bl4ckout31 <bl4ckout31@gmail.com>
2018-07-27 13:06:01 +0200

    Add missing messages in entry_error_messages

--- src/lesson05/src/irq.c
+++ src/lesson05/src/irq.c
@@ -23,7 +23,10 @@ const char *entry_error_messages[] = {
 	"SYNC_INVALID_EL0_32",		
 	"IRQ_INVALID_EL0_32",		
 	"FIQ_INVALID_EL0_32",		
-	"ERROR_INVALID_EL0_32"	
+	"ERROR_INVALID_EL0_32",
+
+	"SYNC_ERROR",
+	"SYSCALL_ERROR"
 };
 
 void enable_interrupt_controller()
