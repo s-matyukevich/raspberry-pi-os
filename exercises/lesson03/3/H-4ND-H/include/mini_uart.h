bc0b5236737009f6eca024d05b04225a2dab2c97
H-4ND-H <hessam.hedieh@hotmail.com>
2018-08-09 06:04:52 -0700

    Copy exercises into proper folders

---
+++ exercises/lesson03/3/H-4ND-H/include/mini_uart.h
@@ -0,0 +1,9 @@
+#ifndef	_MINI_UART_H
+#define	_MINI_UART_H
+
+void uart_init ( void );
+char uart_recv ( void );
+void uart_send ( char c );
+void putc ( void* p, char c );
+
+#endif  /*_MINI_UART_H */
