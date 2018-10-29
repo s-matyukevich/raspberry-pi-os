dbd7ab11fcab7fc2b4d198faaabecb8b7cab5faa
Alexandre Venito <venito@eit.uni-kl.de>
2018-10-29 14:15:37 +0100

    modified:   exercises/lesson03/1/avenito/README.md 	modified:   exercises/lesson03/1/avenito/include/arm/sysregs.h 	modified:   exercises/lesson03/1/avenito/include/entry.h 	modified:   exercises/lesson03/1/avenito/include/peripherals/irq.h 	modified:   exercises/lesson03/1/avenito/src/irq.c 	modified:   exercises/lesson03/1/avenito/src/timer.c

--- exercises/lesson03/1/avenito/include/entry.h
+++ exercises/lesson03/1/avenito/include/entry.h
@@ -13,14 +13,14 @@
 #define FIQ_INVALID_EL1h		6 
 #define ERROR_INVALID_EL1h		7 
 
-#define SYNC_INVALID_EL0_64	    	8 
-#define IRQ_INVALID_EL0_64	    	9 
+#define SYNC_INVALID_EL0_64	   	8 
+#define IRQ_INVALID_EL0_64	   	9 
 #define FIQ_INVALID_EL0_64		10 
-#define ERROR_INVALID_EL0_64		11 
+#define ERROR_INVALID_EL0_64	11 
 
 #define SYNC_INVALID_EL0_32		12 
 #define IRQ_INVALID_EL0_32		13 
 #define FIQ_INVALID_EL0_32		14 
-#define ERROR_INVALID_EL0_32		15 
+#define ERROR_INVALID_EL0_32	15 
 
 #endif
