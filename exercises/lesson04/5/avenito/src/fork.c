25b53dfe39c9934d3bd092d7c5a653b1a77133f8
Alexandre Venito <venito@eit.uni-kl.de>
2018-11-09 15:04:35 +0100

    new file:   exercises/lesson04/5/avenito/Makefile 	new file:   exercises/lesson04/5/avenito/build.bat 	new file:   exercises/lesson04/5/avenito/build.sh 	new file:   exercises/lesson04/5/avenito/include/arm/sysregs.h 	new file:   exercises/lesson04/5/avenito/include/entry.h 	new file:   exercises/lesson04/5/avenito/include/fork.h 	new file:   exercises/lesson04/5/avenito/include/irq.h 	new file:   exercises/lesson04/5/avenito/include/mm.h 	new file:   exercises/lesson04/5/avenito/include/peripherals/base.h 	new file:   exercises/lesson04/5/avenito/include/peripherals/gpio.h 	new file:   exercises/lesson04/5/avenito/include/peripherals/irq.h 	new file:   exercises/lesson04/5/avenito/include/peripherals/timer.h 	new file:   exercises/lesson04/5/avenito/include/peripherals/uart.h 	new file:   exercises/lesson04/5/avenito/include/printf.h 	new file:   exercises/lesson04/5/avenito/include/sched.h 	new file:   exercises/lesson04/5/avenito/include/timer.h 	new file:   exercises/lesson04/5/avenito/include/uart.h 	new file:   exercises/lesson04/5/avenito/include/utils.h 	new file:   exercises/lesson04/5/avenito/saida.txt 	new file:   exercises/lesson04/5/avenito/src/boot.S 	new file:   exercises/lesson04/5/avenito/src/config.txt 	new file:   exercises/lesson04/5/avenito/src/entry.S 	new file:   exercises/lesson04/5/avenito/src/fork.c 	new file:   exercises/lesson04/5/avenito/src/irq.S 	new file:   exercises/lesson04/5/avenito/src/irq.c 	new file:   exercises/lesson04/5/avenito/src/kernel.c 	new file:   exercises/lesson04/5/avenito/src/linker.ld 	new file:   exercises/lesson04/5/avenito/src/mm.S 	new file:   exercises/lesson04/5/avenito/src/mm.c 	new file:   exercises/lesson04/5/avenito/src/printf.c 	new file:   exercises/lesson04/5/avenito/src/sched.S 	new file:   exercises/lesson04/5/avenito/src/sched.c 	new file:   exercises/lesson04/5/avenito/src/timer.c 	new file:   exercises/lesson04/5/avenito/src/uart.c 	new file:   exercises/lesson04/5/avenito/src/utils.S

---
+++ exercises/lesson04/5/avenito/src/fork.c
@@ -0,0 +1,37 @@
+#include "mm.h"
+#include "sched.h"
+#include "entry.h"
+#include "printf.h"
+
+int copy_process(unsigned long fn, unsigned long arg)
+{
+	printf("Copy proccess ...\r\n"); //###
+	
+	preempt_disable();
+	struct task_struct *p;
+
+	p = (struct task_struct *) get_free_page();
+	if (!p)
+		return 1;
+	p->priority = current->priority;
+	p->state = TASK_RUNNING;
+	
+	p->counter = p->priority;
+	
+	p->preempt_count = 1; //disable preemtion until schedule_tail
+
+	p->cpu_context.x19 = fn;
+	p->cpu_context.x20 = arg;
+	p->cpu_context.pc = (unsigned long)ret_from_fork;
+	p->cpu_context.sp = (unsigned long)p + THREAD_SIZE;
+	int pid = nr_tasks++;
+	task[pid] = p;	
+	
+	printf("\ntask[%d] = %d\r\n", pid, p); //###
+	printf("p->priority = current->priority = %d\n\r", current->priority); //###
+	printf("p->state = TASK_RUNNING = %d\r\n", p->state); //###
+	printf("p->counter = p->priority = %d\r\n", p->counter); //###;
+	
+	preempt_enable();
+	return 0;
+}
---
+++ exercises/lesson04/5/avenito/src/sched.c
@@ -0,0 +1,88 @@
+#include "sched.h"
+#include "irq.h"
+#include "printf.h"
+
+static struct task_struct init_task = INIT_TASK;
+struct task_struct *current = &(init_task);
+struct task_struct * task[NR_TASKS] = {&(init_task), };
+int nr_tasks = 1;
+
+void preempt_disable(void)
+{
+	current->preempt_count++;
+}
+
+void preempt_enable(void)
+{
+	current->preempt_count--;
+}
+
+
+void _schedule(void)
+{
+	printf("\n _shedule chamado\r\n"); //###
+	printf("NR_TASKS: %d\r\n", NR_TASKS); //###
+		
+	preempt_disable();
+	int next,c;
+	struct task_struct * p;
+	while (1) {
+		c = -1;
+		next = 0;
+		for (int i = 0; i < NR_TASKS; i++){
+			p = task[i];
+			//printf("\ntask[%d]=%d\n\r", i, task[i]); //###
+			//printf("state=%d\n\r", p->state); //###
+			//printf("counter=%d\n\r", p->counter); //###
+			if (p && p->state == TASK_RUNNING && p->counter > c) {
+				c = p->counter;
+				next = i;
+			}
+		}
+		if (c) {
+			break;
+		}
+		for (int i = 0; i < NR_TASKS; i++) {
+			p = task[i];
+			if (p) {
+				p->counter = (p->counter >> 1) + p->priority;
+			}
+		}
+	}
+	printf("switch_to(task[%d])\n\r;", next);
+	switch_to(task[next]);
+	preempt_enable();
+}
+
+void schedule(void)
+{
+	printf("shedule chamado\r\n"); //###
+	current->counter = 0;
+	_schedule();
+}
+
+void switch_to(struct task_struct * next) 
+{
+	if (current == next) 
+		return;
+	struct task_struct * prev = current;
+	current = next;
+	cpu_switch_to(prev, next);
+}
+
+void schedule_tail(void) {
+	preempt_enable();
+}
+
+
+void timer_tick()
+{
+	--current->counter;
+	if (current->counter>0 || current->preempt_count >0) {
+		return;
+	}
+	current->counter=0;
+	enable_irq();
+	_schedule();
+	disable_irq();
+}
