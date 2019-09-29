c59a77bc5d4d5cb97a2797b277d1f71053254f8b
Zhoutaoccu <zhoutaoccu@qq.com>
2019-09-30 02:07:04 +0800

    Adatp to Pi 4

--- src/lesson01/include/peripherals/base.h
+++ src/lesson01/include/peripherals/base.h
@@ -1,6 +1,6 @@
 #ifndef	_P_BASE_H
 #define	_P_BASE_H
 
-#define PBASE 0x3F000000
+#define PBASE 0xFE000000 /* Pi 3: 0x3F000000 */
 
 #endif  /*_P_BASE_H */
--- src/lesson01/src/mini_uart.c
+++ src/lesson01/src/mini_uart.c
@@ -49,7 +49,7 @@ void uart_init ( void )
 	put32(AUX_MU_IER_REG,0);                //Disable receive and transmit interrupts
 	put32(AUX_MU_LCR_REG,3);                //Enable 8 bit mode
 	put32(AUX_MU_MCR_REG,0);                //Set RTS line to be always high
-	put32(AUX_MU_BAUD_REG,270);             //Set baud rate to 115200
+	put32(AUX_MU_BAUD_REG,541);             //Set baud rate to 115200, new GPU clock rate is 500MHz, not 250MHz
 
 	put32(AUX_MU_CNTL_REG,3);               //Finally, enable transmitter and receiver
 }
