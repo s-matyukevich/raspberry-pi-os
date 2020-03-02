0212fd3639b99ecfaf0df6e180c2548e4fca812a
justapig9020 <justapig9020@gmail.com>
2020-03-02 09:17:48 +0800

    modify bit mask at include/arm/sysregs.h

--- exercises/lesson01/1/justapig9020/include/mm.h
+++ exercises/lesson01/1/justapig9020/include/mm.h
@@ -1,14 +1,14 @@
 #ifndef    _MM_H
 #define    _MM_H
 
-#define PAGE_SHIFT             12
-#define TABLE_SHIFT             9
-#define SECTION_SHIFT            (PAGE_SHIFT + TABLE_SHIFT)
+#define PAGE_SHIFT 12
+#define TABLE_SHIFT 9
+#define SECTION_SHIFT (PAGE_SHIFT + TABLE_SHIFT)
 
-#define PAGE_SIZE               (1 << PAGE_SHIFT)    
-#define SECTION_SIZE            (1 << SECTION_SHIFT)    
+#define PAGE_SIZE (1 << PAGE_SHIFT)
+#define SECTION_SIZE (1 << SECTION_SHIFT)
 
-#define LOW_MEMORY                  (2 * SECTION_SIZE)
+#define LOW_MEMORY (SECTION_SIZE << 1)
 
 #ifndef __ASSEMBLER__
 
--- src/lesson02/include/arm/sysregs.h
+++ src/lesson02/include/arm/sysregs.h
@@ -6,14 +6,14 @@
 // ***************************************
 
 #define SCTLR_RESERVED                  (3 << 28) | (3 << 22) | (1 << 20) | (1 << 11)
-#define SCTLR_EE_LITTLE_ENDIAN          (0 << 25)
-#define SCTLR_EOE_LITTLE_ENDIAN         (0 << 24)
-#define SCTLR_I_CACHE_DISABLED          (0 << 12)
-#define SCTLR_D_CACHE_DISABLED          (0 << 2)
-#define SCTLR_MMU_DISABLED              (0 << 0)
+#define SCTLR_EE_LITTLE_ENDIAN          ~(1 << 25)
+#define SCTLR_EOE_LITTLE_ENDIAN         ~(1 << 24)
+#define SCTLR_I_CACHE_DISABLED          ~(1 << 12)
+#define SCTLR_D_CACHE_DISABLED          ~(1 << 2)
+#define SCTLR_MMU_DISABLED              ~(1 << 0)
 #define SCTLR_MMU_ENABLED               (1 << 0)
 
-#define SCTLR_VALUE_MMU_DISABLED	(SCTLR_RESERVED | SCTLR_EE_LITTLE_ENDIAN | SCTLR_I_CACHE_DISABLED | SCTLR_D_CACHE_DISABLED | SCTLR_MMU_DISABLED)
+#define SCTLR_VALUE_MMU_DISABLED	(SCTLR_RESERVED & SCTLR_EE_LITTLE_ENDIAN & SCTLR_I_CACHE_DISABLED & SCTLR_D_CACHE_DISABLED & SCTLR_MMU_DISABLED)
 
 // ***************************************
 // HCR_EL2, Hypervisor Configuration Register (EL2), Page 2487 of AArch64-Reference-Manual.
