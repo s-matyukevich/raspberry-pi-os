d8858a807815fd0948c4cc05d92afd172c079a60
stefanji <jidaoyang@gmail.com>
2020-05-03 17:06:59 +0800

    feat: run on qemu script

--- exercises/lesson01/4/stefanji/run_on_qemu.sh
+++ exercises/lesson01/4/stefanji/run_on_qemu.sh
@@ -1,5 +1,14 @@
 #! /usr/bin/env bash
 
+path='../../../../src/lesson01/'
+
+# build lesson01
+echo "cd $path"
+cd $path
+./build.sh $1
+
+# run on qemu
+echo "Will running on qemu, ctl-c to exit qemu process"
 qemu-system-aarch64 -m 128 \
  -M raspi3 \
  -cpu cortex-a53 \
