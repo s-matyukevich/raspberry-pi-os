7d5c7875d398724be4b62009a64c776aad29b782
Michael Szediwy <michael@szediwy.ch>
2021-01-14 21:43:04 +0100

    Lesson 02 - Exercise 2

---
+++ exercises/lesson02/2/szediwy/src/kernel.c
@@ -0,0 +1,33 @@
+#include "custom_printf.h"
+#include "utils.h"
+#include "mini_uart.h"
+
+void kernel_main(unsigned long processor_index)
+{
+	static unsigned int current_processor_index = 0;
+
+	if (processor_index == 0)
+	{
+		uart_init();
+		init_printf(0, putc);	
+	}
+
+	while (processor_index != current_processor_index)
+		;
+
+	int exception_level = get_el();
+	printf("{CPU: %d, Exception level: %d} \r\n", processor_index, exception_level);
+
+	current_processor_index++;
+
+	if (processor_index == 0)
+	{
+		// if current_processor_index == 4 then all processors send message
+		while (current_processor_index != 4)
+			;
+		for (;;)
+		{
+			uart_send(uart_recv());
+		}
+	}
+}
