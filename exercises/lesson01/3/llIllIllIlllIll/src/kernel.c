11d11d98051a66aaf3966aa481a7e5f3e579c201
softpudding <yangxiao1098@gmail.com>
2021-06-14 06:17:50 -0700

    very successful version using delay

--- exercises/lesson01/3/llIllIllIlllIll/src/kernel.c
+++ exercises/lesson01/3/llIllIllIlllIll/src/kernel.c
@@ -2,18 +2,12 @@
 
 void kernel_main(unsigned long long processor_id)
 {
-	static unsigned long long current_speaking_id = 0;
-
 	if(processor_id == 0)
 		uart_init();
 
-	while(processor_id != current_speaking_id)
-		;
 	uart_send_string("Hello, from processor");
 	uart_send('0'+processor_id);
 	uart_send_string("\r\n");
-
-	current_speaking_id++;
 
 	if(processor_id == 0){
 		while (1) {
