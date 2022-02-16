87d399d97db8126c3512d55cc1eccd7eb851850f
CrimsonValor <68930098+CrimsonValor@users.noreply.github.com>
2022-02-16 12:19:38 -0500

    Update mini_uart.c
    Fixed type line 55.

--- src/lesson01/src/mini_uart.c
+++ src/lesson01/src/mini_uart.c
@@ -52,7 +52,7 @@ void uart_init ( void )
 	put32(AUX_MU_IER_REG,0);                //Disable receive and transmit interrupts
 	put32(AUX_MU_LCR_REG,3);                //Enable 8 bit mode
 	put32(AUX_MU_MCR_REG,0);                //Set RTS line to be always high
-	put32(AUX_MU_BAUD_REG,baudreate_reg);             //Set baud rate to 115200
+	put32(AUX_MU_BAUD_REG,baudrate_reg);             //Set baud rate to 115200
 
 	put32(AUX_MU_CNTL_REG,3);               //Finally, enable transmitter and receiver
 }
